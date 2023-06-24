326874934dc876f4961c68098c25233a713e250c
Sabina Pacheco <129625809+sabinamunoz@users.noreply.github.com>
2023-06-24 10:26:27 +0100

    Add files via upload

--- SQL Queries 4.sql
+++ SQL Queries 4.sql
@@ -8,15 +8,16 @@ select * from film where title like '%ARMAGEDDON%';
 -- 4. Get all films with APOLLO in the title
 select * from film where title like '%APOLLO%'; 
 -- 5. Get all films which title ends with APOLLO.
+select * from film where title like '%APOLLO';
 -- 6. Get all films with word DATE in the title.
 select * from film where title like '%DATE%';
 -- 7. Get 10 films with the longest title.
 select *, length(title) as title_length from film
-order by title_length asc 
+order by title_length desc 
 limit 10;
 -- 8.Get 10 the longest films.
 select * from film 
-order by length asc
+order by length desc
 limit 10;
 -- 9. How many films include Behind the Scenes content?
 select * from film where special_features like '%Behind the Scenes%';
